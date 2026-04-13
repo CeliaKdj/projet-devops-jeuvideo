e25e77f7624c0fad6a3f5002c86f7cb97ae7790c
CeliaKdj <celia.kadjo@epitech.eu>
2026-04-13 10:30:14 +0200

    update: utils.js var to const

--- games/Two-Ships-ITN/src/utils.js
+++ games/Two-Ships-ITN/src/utils.js
@@ -1,11 +1,12 @@
-export var flow = (...fns) => initialValue =>
+// fonction qui cherche des fonctions qui manipulent des couleurs, des angles ou des chaines de caractère
+export const flow = (...fns) => initialValue =>
   fns.reduce((value, fn) => fn(value), initialValue);
 
-export var rearg = fn => (...args) => value => fn(value, ...args);
+export const rearg = fn => (...args) => value => fn(value, ...args);
 
-export var remove = (array, element) => {
-  var index = array.indexOf(element);
+export const remove = (array, element) => {
+  const index = array.indexOf(element);
   if (index >= 0) array.splice(index, 1);
 };
 
-export var sample = array => array[(Math.random() * array.length) | 0];
+export const sample = array => array[(Math.random() * array.length) | 0];
